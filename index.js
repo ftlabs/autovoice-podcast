bae5e7b3c45033139ed248717e55a3f35fdf9959
Chris Gathercole <chris.gathercole@ft.com>
2017-02-28 10:53:06 +0000

    first refactor: remove the absorber actions

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const dotenv = require('dotenv').config();
-require('./bin/lib/minimum-server');
+const debug = require('debug')('absorber:index');
+const express = require('express');
+const app = express();
+// const absorber = require('./bin/lib/absorb.js');
 
-const absorber = require('./bin/lib/absorb.js');
+app.get('/', (req, res) => {
+	res.status(200).end();
+});
 
-absorber.poll((1000 * 60) * 2, true);
+app.get('/__gtg', (req, res) => {
+	res.status(200).end();
+});
+
+app.listen(process.env.PORT, function(){
+	debug('Server is listening on port', process.env.PORT);
+});
