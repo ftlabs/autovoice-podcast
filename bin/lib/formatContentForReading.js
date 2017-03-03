2d9759aef77a84e7ed3e9b351974b58feaa66406
Chris Gathercole <chris.gathercole@ft.com>
2017-03-03 15:40:54 +0000

    tweaking ACRONYMS to work

--- bin/lib/formatContentForReading.js
+++ bin/lib/formatContentForReading.js
@@ -4,21 +4,27 @@ const striptags = require('striptags');
 const ACRONYMS = [
 	'BBC',
 	'EU',
+	'FBI',
 	'FT',
 	'ICE',
 	'IMF',
+	'IPO',
 	'LSE',
 	'MTS',
 	'NAR',
+	'NYSE',
 	'UK',
 	'US',
 	'WPP',
-	'WTO',
+	'WTO'
 ];
 
-const ACRONYMS_REGEXP = '\b(' + ACRONYMS.join('|') + ')\b';
+const ACRONYMS_PATTERN = `\\b(${ACRONYMS.join('|') })\\b`;
+const ACRONYMS_REGEXP  = new RegExp(ACRONYMS_PATTERN, 'g');
+debug(`ACRONYMS_REGEXP=${ACRONYMS_REGEXP}`);
 
 module.exports = function(itemData) {
+
 	let texts = [
 		`This article is narrated by ${itemData['narrator-id']}, as part of an ongoing experiment with artificial voices.`,
 		itemData.title
@@ -33,22 +39,24 @@ module.exports = function(itemData) {
 
 	let content = itemData.content
 	.replace(/<span[^>]+>Sign up to receive FirstFT by email <a[^>]+>here<\/a>/g, '')
-	.replace(/<p[^>]*>/g, '\n')
-	.replace(/<\/p[^>]*>/g, '\n')
+	.replace(/<p[^>]*>/g, '. ')
+	.replace(/<\/p[^>]*>/g, '. ')
 	;
 
 	content = striptags( content )
+	.replace(ACRONYMS_REGEXP, match => { return match.split('').join('.') })
 	.replace(/"/g, "\'")
 	.replace(/\\n(\\n)+/g, '\n')
+	.replace(/\. (\. )+/g, '. ')
 	;
 
 	// also parse/rewrite some of the firstFT-specific text e.g. the attributions in brackets
 
 	texts.push(content);
 
-	texts.push(`This article was ${itemData.title}.`);
+	texts.push(`This article was titled ${itemData.title}.`);
 	if (itemData.author) {
-		texts.push(`Written by ${itemData.author}`);
+		texts.push(`This article was written by ${itemData.author}.`);
 	}
 
   texts.push(`This article was narrated by ${itemData['narrator-id']}, as part of an ongoing experiment with artificial voices.`);
