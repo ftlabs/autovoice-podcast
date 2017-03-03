66865868b57f510ac9d52b95d9afff280e70443b
Chris Gathercole <chris.gathercole@ft.com>
2017-03-03 14:05:15 +0000

    refactored const reformat     = require('./reformat');

---
+++ bin/lib/formatContentForReading.js
@@ -0,0 +1,43 @@
+const debug = require('debug')('bin:lib:formatContentForReading');
+
+const reformat     = require('./reformat');
+
+const     ffIntroRegexp = new RegExp('<span[^>]+>Sign up to receive FirstFT by email <a[^>]+>here<\/a>');
+const speechMarksRegexp = new RegExp('"', "g");
+const    newlinesRegexp = new RegExp('\\n(\\n)+', "g");
+
+module.exports = function(itemData) {
+	let texts = [
+		`This article is narrated by ${itemData['narrator-id']}, as part of an ongoing experiment with artificial voices.`,
+		itemData.title
+	];
+
+	if (itemData.author) {
+		texts.push(`Written by ${itemData.author}`);
+	}
+
+	// <span class="ft-bold">Sign up to receive FirstFT by email
+	// <a title="FirstFT" href="http://nbe.ft.com/nbe/profile.cfm?firstft=Y">here</a> </span>
+
+	if (itemData.content.match(ffIntroRegexp)) {
+		debug(`formatContentForReading: matched ffIntroRegexp in article w/title=${itemData.title}`);
+	}
+
+	let content = itemData.content.replace(ffIntroRegexp, "");
+	content = reformat( content );
+	content = content.replace(speechMarksRegexp, "\'");
+	content = content.replace(newlinesRegexp, '\n');
+
+	// also parse/rewrite some of the firstFT-specific text e.g. the attributions in brackets
+
+	texts.push(content);
+
+	texts.push(`This article was ${itemData.title}.`);
+	if (itemData.author) {
+		texts.push(`Written by ${itemData.author}`);
+	}
+
+  texts.push(`This article was narrated by ${itemData['narrator-id']}, as part of an ongoing experiment with artificial voices.`);
+
+	return texts.join("\n");
+}
